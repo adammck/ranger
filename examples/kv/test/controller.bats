aced8ce310bc024084cf49572680fbafc17377d8
Adam Mckaig <adam.mckaig@gmail.com>
2022-02-12 20:21:28 -0500

    Add split test; doesn't work

--- examples/kv/test/controller.bats
+++ examples/kv/test/controller.bats
@@ -52,10 +52,6 @@ teardown() {
     # Move the range from node 1 to node 2.
     run bin/client.sh 9000 ranger.Balancer.Move '{"range": {"key": 1}, "node": "8002"}'
 
-    # TODO: Make the RPC handler (optionally?) block until the op is done, so we
-    # don't have to do dumb things like this.
-    sleep 1
-
     # Check that the range is gone from node 1.
     run bin/client.sh 8001 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
     assert_failure
@@ -63,4 +59,36 @@ teardown() {
     # Check that it is now available on node 2.
     run bin/client.sh 8002 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
     assert_success
+}
+
+@test "split" {
+    start_node 8001
+    start_node 8002
+    start_node 8003
+    start_controller 9000
+
+    # Write a key on either side of the split.
+    run bin/client.sh 8001 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
+    assert_success
+    run bin/client.sh 8001 kv.KV.Put '{"key": "'$c'", "value": "'$yyy'"}'
+    assert_success
+
+    # Split the range from node 1 to nodes 2 and three.
+    run bin/client.sh 9000 ranger.Balancer.Split '{"range": {"key": 1}, "boundary": "'$b'", "node_left": "8002", "node_right": "8003"}'
+
+    # Check that the range is gone from node 1.
+    run bin/client.sh 8001 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
+    assert_failure
+
+    # Check that the left side is available on node 1, but the right is not.
+    run bin/client.sh 8002 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
+    assert_success
+    run bin/client.sh 8002 kv.KV.Put '{"key": "'$c'", "value": "'$zzz'"}'
+    assert_failure
+
+    # Check that the right side is available on node 2, but the left is not.
+    run bin/client.sh 8003 kv.KV.Put '{"key": "'$c'", "value": "'$zzz'"}'
+    assert_success
+    run bin/client.sh 8003 kv.KV.Put '{"key": "'$a'", "value": "'$zzz'"}'
+    assert_failure
 }
--- examples/kv/test/test_helper.bash
+++ examples/kv/test/test_helper.bash
@@ -1,19 +1,19 @@
 cmds=()
 
 # keys
-a=$(echo -n a | base64); export a;
-a1=$(echo -n a1 | base64); export a1;
-a2=$(echo -n a2 | base64); export a2;
-b=$(echo -n b | base64); export b;
-b1=$(echo -n b1 | base64); export b1;
-b2=$(echo -n b2 | base64); export b2;
-c=$(echo -n c | base64); export c;
+a=$(echo -n a | base64); export a
+a1=$(echo -n a1 | base64); export a1
+a2=$(echo -n a2 | base64); export a2
+b=$(echo -n b | base64); export b
+b1=$(echo -n b1 | base64); export b1
+b2=$(echo -n b2 | base64); export b2
+c=$(echo -n c | base64); export c
 
 # vals
-zzz=$(echo -n zzz | base64); export zzz;
-yyy=$(echo -n yyy | base64); export yyy;
-xxx=$(echo -n xxx | base64); export xxx;
-www=$(echo -n www | base64); export www;
+zzz=$(echo -n zzz | base64); export zzz
+yyy=$(echo -n yyy | base64); export yyy
+xxx=$(echo -n xxx | base64); export xxx
+www=$(echo -n www | base64); export www
 
 # Fail if the given port number is currently in use. This is better than trying
 # to run some command and failing with a weird error.
@@ -55,14 +55,16 @@ start_cmd() {
 
     assert_port_available "$port"
     "$@" &
-    defer_stop_cmds "$!"
-    run wait-port "$port"
+    local pid=$!
+
+    defer_stop_cmds "$pid"
+    wait_port "$port" "$pid"
 }
 
 stop_cmd() {
     #>&3 echo "# stop_cmd $@"
     if test -n "$1"; then # if non-empty
-        if kill -s 0 "$1"; then # and still running
+        if kill -s 0 "$1" 2>/dev/null; then # and still running
             kill "$1"
         fi
     fi
@@ -77,5 +79,24 @@ stop_cmds() {
     #>&3 echo "# stop_cmds $1"
     for pid in "${cmds[@]}"; do
         stop_cmd "$pid"
+    done
+}
+
+# Block until either the given port is listening, or the given PID is no longer
+# running.
+wait_port() {
+    local port=$1
+    local pid=$2
+
+    while : ; do
+        if ! kill -s 0 "$pid" 2>/dev/null; then
+            fail "program terminated while waiting for port $port to listen"
+        fi
+
+        if nc -z localhost "$port"; then
+            return 0;
+        fi
+
+        sleep 0.1
     done
 }
