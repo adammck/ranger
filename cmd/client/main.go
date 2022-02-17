6cc2632b20f60d090fa204597f4c671e07e0ca0a
Adam Mckaig <adam.mckaig@gmail.com>
2022-02-16 20:47:19 -0500

    Add ranger.Split to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"os"
@@ -24,12 +26,14 @@ func main() {
 		fmt.Fprintf(w, "  - range <rangeID>\n")
 		fmt.Fprintf(w, "  - node <nodeID>\n")
 		fmt.Fprintf(w, "  - move <rangeID> <nodeID>\n")
+		fmt.Fprintf(w, "  - split <rangeID> <boundary> <nodeID> <nodeID>\n")
 		fmt.Fprintf(w, "\n")
 		fmt.Fprintf(w, "Flags:\n")
 		flag.PrintDefaults()
 	}
 
 	addr := flag.String("addr", "localhost:5000", "controller address")
+	printReq := flag.Bool("request", false, "print gRPC request instead of sending it")
 	flag.Parse()
 
 	if flag.NArg() == 0 {
@@ -64,7 +68,7 @@ func main() {
 		}
 
 		client := pb.NewDebugClient(conn)
-		cmdRange(client, ctx, rID)
+		cmdRange(*printReq, client, ctx, rID)
 
 	case "node", "n":
 		if flag.NArg() != 2 {
@@ -73,7 +77,7 @@ func main() {
 		}
 
 		client := pb.NewDebugClient(conn)
-		cmdNode(client, ctx, flag.Arg(1))
+		cmdNode(*printReq, client, ctx, flag.Arg(1))
 
 	case "move", "m":
 		if flag.NArg() != 3 {
@@ -88,7 +92,37 @@ func main() {
 		}
 
 		client := pb.NewBalancerClient(conn)
-		cmdMove(client, ctx, rID, flag.Arg(2))
+		cmdMove(*printReq, client, ctx, rID, flag.Arg(2))
+
+	case "split", "s":
+		if flag.NArg() != 5 {
+			fmt.Fprintf(w, "Usage: %s split <rangeID> <boundary> <nodeID> <nodeID>\n", os.Args[0])
+			os.Exit(1)
+		}
+
+		boundary := []byte(flag.Arg(2))
+
+		// If the boundary is prefixed with 'b64:' then decode the rest.
+		// Sometimes we want to split at points which are not printable chars.
+		p := []byte("b64:")
+		if bytes.HasPrefix(boundary, p) {
+			b := bytes.TrimPrefix(boundary, p)
+			_, err = base64.StdEncoding.Decode(boundary, b)
+
+			if err != nil {
+				fmt.Fprintf(w, "Invalid base64-encoded boundary: %v\n", b)
+				os.Exit(1)
+			}
+		}
+
+		rID, err := strconv.ParseUint(flag.Arg(1), 10, 64)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid rangeID: %v\n", err)
+			os.Exit(1)
+		}
+
+		client := pb.NewBalancerClient(conn)
+		cmdSplit(*printReq, client, ctx, rID, boundary, flag.Arg(3), flag.Arg(4))
 
 	default:
 		flag.Usage()
@@ -96,12 +130,22 @@ func main() {
 	}
 }
 
-func cmdRange(client pb.DebugClient, ctx context.Context, rID uint64) {
+func cmdRange(printReq bool, client pb.DebugClient, ctx context.Context, rID uint64) {
 	w := flag.CommandLine.Output()
 
-	res, err := client.Range(ctx, &pb.RangeRequest{Range: &pb.Ident{
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	req := &pb.RangeRequest{Range: &pb.Ident{
 		Key: rID,
-	}})
+	}}
+
+	if printReq {
+		output(req)
+		return
+	}
+
+	res, err := client.Range(ctx, req)
 
 	if err != nil {
 		fmt.Fprintf(w, "Debug.Range returned: %v\n", err)
@@ -111,10 +155,20 @@ func cmdRange(client pb.DebugClient, ctx context.Context, rID uint64) {
 	output(res)
 }
 
-func cmdNode(client pb.DebugClient, ctx context.Context, nID string) {
+func cmdNode(printReq bool, client pb.DebugClient, ctx context.Context, nID string) {
 	w := flag.CommandLine.Output()
 
-	res, err := client.Node(ctx, &pb.NodeRequest{Node: nID})
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	req := &pb.NodeRequest{Node: nID}
+
+	if printReq {
+		output(req)
+		return
+	}
+
+	res, err := client.Node(ctx, req)
 
 	if err != nil {
 		fmt.Fprintf(w, "Debug.Node returned: %v\n", err)
@@ -124,21 +178,58 @@ func cmdNode(client pb.DebugClient, ctx context.Context, nID string) {
 	output(res)
 }
 
-func cmdMove(client pb.BalancerClient, ctx context.Context, rID uint64, nID string) {
+func cmdMove(printReq bool, client pb.BalancerClient, ctx context.Context, rID uint64, nID string) {
 	w := flag.CommandLine.Output()
 
 	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
-	res, err := client.Move(ctx, &pb.MoveRequest{
+	req := &pb.MoveRequest{
 		Range: &pb.Ident{
 			Key: rID,
 		},
 		Node: nID,
-	})
+	}
+
+	if printReq {
+		output(req)
+		return
+	}
+
+	res, err := client.Move(ctx, req)
 
 	if err != nil {
 		fmt.Fprintf(w, "Debug.Move returned: %v\n", err)
+		os.Exit(1)
+	}
+
+	output(res)
+}
+
+func cmdSplit(printReq bool, client pb.BalancerClient, ctx context.Context, rID uint64, boundary []byte, nID1, nID2 string) {
+	w := flag.CommandLine.Output()
+
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	req := &pb.SplitRequest{
+		Range: &pb.Ident{
+			Key: rID,
+		},
+		Boundary:  boundary,
+		NodeLeft:  nID1,
+		NodeRight: nID2,
+	}
+
+	if printReq {
+		output(req)
+		return
+	}
+
+	res, err := client.Split(ctx, req)
+
+	if err != nil {
+		fmt.Fprintf(w, "Debug.Split returned: %v\n", err)
 		os.Exit(1)
 	}
 
