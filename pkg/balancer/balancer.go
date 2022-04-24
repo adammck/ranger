926e9a18f6a57f9db89dbdd003a96f2a78768606
Adam Mckaig <adam.mckaig@gmail.com>
2022-04-24 15:10:03 -0400

    Move ops into own file

--- pkg/balancer/balancer.go
+++ pkg/balancer/balancer.go
@@ -15,25 +15,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-// TODO: Split this into Add, Remove
-type OpMove struct {
-	Range ranje.Ident
-	Src   string
-	Dest  string
-	Err   chan error
-}
-
-type OpSplit struct {
-	Range ranje.Ident
-	Key   ranje.Key
-}
-
-// TODO: Allow operator to specify which node to target?
-type OpJoin struct {
-	Left  ranje.Ident
-	Right ranje.Ident
-}
-
 type Balancer struct {
 	cfg  config.Config
 	ks   *ranje.Keyspace
