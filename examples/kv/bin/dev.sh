4426a9e2602ccd1eed2756b584917d8e29b2dae4
Adam Mckaig <adam.mckaig@gmail.com>
2022-04-24 23:12:15 -0400

    rename client to rangerctl

--- examples/kv/bin/dev.sh
+++ examples/kv/bin/dev.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 go build # kv
+go build "$(dirname "$0")"/../../../cmd/rangerctl
 go build "$(dirname "$0")"/../../../cmd/rangerd
 foreman start -m controller=1,proxy=1,node=3
