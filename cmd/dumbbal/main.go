f33ad304accc589bef36cc606717145a48a5d895
Adam Mckaig <adam.mckaig@gmail.com>
2022-05-05 21:40:43 -0400

    Add dumbbal: a dumb balancer

---
+++ cmd/dumbbal/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// Create a debug client (like rangerctl)
+
+	// Fetch the current state of all the ranges
+
+	// Find any ranges higher than the threshold
+
+	// Send RPCs (in parallel) to split each one
+
+	// Find any adjacent range pairs lower than the threshold
+
+	// Send RPCs (in parallel) to join each pair
+
+	// Exit
+}
