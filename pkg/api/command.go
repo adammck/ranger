b6e7611bca5f0b73bab2d9bd359ec1eb4ad673bf
Adam Mckaig <adam.mckaig@gmail.com>
2023-06-22 00:56:30 -0400

    Add expire time to Command
    This breaks many tests, because nobody is populating Expires yet!
    
    Also, this is only relevant when Action==Activate, which is a bit ugly.
    But I don't think we need separate command types for each command. Maybe
    if I find myself doing this a second time for some other action.

--- pkg/api/command.go
+++ pkg/api/command.go
@@ -2,22 +2,43 @@ package api
 
 import (
 	"fmt"
+	"strings"
+	"time"
 )
 
 type Command struct {
 	RangeIdent RangeID
 	NodeIdent  NodeID
 	Action     Action
+
+	// Must be non-zero when Action == Activate, otherwise must be zero.
+	Expires time.Time
 }
 
-func (t Command) String() string {
-	return fmt.Sprintf("%s(R%d, %s)", t.Action, t.RangeIdent, t.NodeIdent)
+func (c Command) String() string {
+	s := make([]string, 2, 3)
+	s[0] = fmt.Sprintf("R%d", c.RangeIdent)
+	s[1] = string(c.NodeIdent)
+
+	if c.Action == Activate {
+		s = append(s, fmt.Sprint(c.Expires.Unix()))
+
+		// This should never happen, because the command should never have been
+		// allowed to be constructed this way. It's just a runtime assert.
+		// TODO: Add a constructor with an err field.
+		// TODO: Remove this check eventually.
+		if c.Expires.IsZero() {
+			panic("activate command with no lease")
+		}
+	}
+
+	return fmt.Sprintf("%s(%s)", c.Action, strings.Join(s, ", "))
 }
 
-func (t Command) Less(other Command) bool {
-	if t.RangeIdent != other.RangeIdent {
-		return t.RangeIdent < other.RangeIdent
+func (c Command) Less(other Command) bool {
+	if c.RangeIdent != other.RangeIdent {
+		return c.RangeIdent < other.RangeIdent
 	} else {
-		return t.NodeIdent < other.NodeIdent
+		return c.NodeIdent < other.NodeIdent
 	}
 }
